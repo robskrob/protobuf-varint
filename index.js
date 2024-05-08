4d94b58a2fd4db56963a229e246a63981a09ae1c
robskrob <jewell.robertp@gmail.com>
2024-05-08 18:02:02 -0400

    wip: encode

--- index.js
+++ index.js
@@ -3,9 +3,34 @@ const path = require('path')
 const { Buffer } = require('node:buffer');
 
 
+let hexBuilder = '';
+const buildHex = (number) => {
+  const quotient = Math.floor(number / 16);
+  const remainder = number % 16;
+  const v = num2hex(remainder);
+
+  hexBuilder = v + hexBuilder;
+
+  if (1 > quotient) {
+    return hexBuilder;
+  } else {
+    return buildHex(quotient);
+  }
+}
 
 
-const encode = async () => {
+const encode = (number) => {
+
+  const fullHex = buildHex(number);
+  // let binary = '';
+  // fullHex.split('').forEach(h => {
+  //   const bin = (Number(hex2number(h)) >>> 0).toString(2)
+  //   binary += (Number(hex2number(h)) >>> 0).toString(2);
+  // })
+
+  // console.log(binary)
+
+  return fullHex
 }
 
 const decode = async () => {
@@ -43,63 +68,34 @@ const decode = async () => {
 }
 
 (async () => {
-  await decode();
+  // await decode();
+  const coded = encode(150);
+  console.log(coded);
 })()
 
-function hex2bin(hex){
-    hex = hex.replace("0x", "").toLowerCase();
-    var out = "";
-    for(var c of hex) {
-        switch(c) {
-            case '0': out += "0000"; break;
-            case '1': out += "0001"; break;
-            case '2': out += "0010"; break;
-            case '3': out += "0011"; break;
-            case '4': out += "0100"; break;
-            case '5': out += "0101"; break;
-            case '6': out += "0110"; break;
-            case '7': out += "0111"; break;
-            case '8': out += "1000"; break;
-            case '9': out += "1001"; break;
-            case 'a': out += "1010"; break;
-            case 'b': out += "1011"; break;
-            case 'c': out += "1100"; break;
-            case 'd': out += "1101"; break;
-            case 'e': out += "1110"; break;
-            case 'f': out += "1111"; break;
-            default: return "";
-        }
-    }
+function num2hex(num){
+  var out = "";
+  switch(num) {
+    case 0: out += '0'; break;
+    case 1: out += '1'; break;
+    case 2: out += '2'; break;
+    case 3: out += '3'; break;
+    case 4: out += '4'; break;
+    case 5: out += '5'; break;
+    case 6: out += '6'; break;
+    case 7: out += '7'; break;
+    case 8: out += '8'; break;
+    case 9: out += '9'; break;
+    case 10:out += 'a'; break;
+    case 11:out += 'b'; break;
+    case 12:out += 'c'; break;
+    case 13:out += 'd'; break;
+    case 14:out += 'e'; break;
+    case 15:out += 'f'; break;
+    default: return "";
+  }
 
-    return out;
-}
-
-function hex2num(hex){
-    hex = hex.replace("0x", "").toLowerCase();
-    var out = "";
-    for(var c of hex) {
-        switch(c) {
-            case '0': out += 0; break;
-            case '1': out += 1; break;
-            case '2': out += 2; break;
-            case '3': out += 3; break;
-            case '4': out += 4; break;
-            case '5': out += 5; break;
-            case '6': out += 6; break;
-            case '7': out += 7; break;
-            case '8': out += 8; break;
-            case '9': out += 9; break;
-            case 'a': out += 10; break;
-            case 'b': out += 11; break;
-            case 'c': out += 12; break;
-            case 'd': out += 13; break;
-            case 'e': out += 14; break;
-            case 'f': out += 15; break;
-            default: return "";
-        }
-    }
-
-    return out;
+  return out;
 }
 
 function hex2number(hex){
