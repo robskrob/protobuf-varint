658e6b113d3706aa2dc30062f5d19f9fa2c0e6b5
robskrob <jewell.robertp@gmail.com>
2024-06-21 10:02:24 -0400

    implement decode

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// "Write a program that can encode 64-bit unsigned integers
+// into the Protocol Buffers varint encoding."
+
 const fs = require('fs')
 const path = require('path')
 const { Buffer } = require('node:buffer');
@@ -22,29 +25,28 @@ const buildHex = (number) => {
 const encode = (number) => {
 
   const fullHex = buildHex(number);
-  // let binary = '';
-  // fullHex.split('').forEach(h => {
-  //   const bin = (Number(hex2number(h)) >>> 0).toString(2)
-  //   binary += (Number(hex2number(h)) >>> 0).toString(2);
-  // })
 
-  // console.log(binary)
+  const binString = fullHex.split(',').reduce((binary, h) => {
 
-  return fullHex
+    binary += hex2bin(h);
+    return binary
+  }, "")
+
+  console.log(binString)
+  if (binString[0] === '1') {
+    return fullHex + "01";
+  } else {
+    return "0" + fullHex
+  }
 }
 
 const decode = async () => {
-  const calculateTrackingPosition = (tracker) => {
-    return (8 * tracker) - 1;
-  }
+  const powerOfSixteen = 16;
 
   const calculateDecimal = (number, power) => {
     const n = Number(number);
-    return n * 16 ** power;
-  }
-
-  const calculatePower = (length, i) => {
-    return (length - 1) - i;
+    console.log(`${n} * ${powerOfSixteen} ** ${power}`)
+    return n * powerOfSixteen ** power;
   }
 
   const encoded = await fs.readFileSync(path.resolve(__dirname, 'maxint.uint64'));
@@ -52,25 +54,31 @@ const decode = async () => {
   const hexString = encoded.toString("hex");
   let decimal = []
   let hexLength = hexString.length;
+  let highestPower = hexLength - 1;
 
-  let bitProgression = 15;
+  // let bitProgression = 15;
 
-  let passes = 1;
-  const hexNumeArr = []
-  console.log("bits", hexLength)
+  // let passes = 1;
   console.log("hexString", hexString)
+  console.log("number of bytes", hexLength)
+  console.log("highest power", highestPower)
   for (var i = 0; i < hexLength; i++) {
-    decimal.push(calculateDecimal(hex2number(hexString[i]), calculatePower(hexLength, i)));
+    decimal.push(calculateDecimal(hex2number(hexString[i]), highestPower - i));
   }
 
   console.log("decimal", decimal)
-  console.log("sum", decimal.reduce((a, b) => a + b, 0))
+  const sum = decimal.reduce((a, b) => a + b, 0);
+  console.log("sum", sum)
+
+  return sum;
 }
 
 (async () => {
-  // await decode();
-  const coded = encode(150);
-  console.log(coded);
+  // encode(150) == b'\x96\x01'
+  // decode(b'\x96\x01') == 150
+  await decode();
+  // const coded = encode(1);
+  // console.log(coded);
 })()
 
 function num2hex(num){
@@ -124,4 +132,32 @@ function hex2number(hex){
     }
 
     return Number(out);
+}
+
+function hex2bin(hex){
+    hex = hex.replace("0x", "").toLowerCase();
+    var out = "";
+    for(var c of hex) {
+        switch(c) {
+            case '0': out += "0000"; break;
+            case '1': out += "0001"; break;
+            case '2': out += "0010"; break;
+            case '3': out += "0011"; break;
+            case '4': out += "0100"; break;
+            case '5': out += "0101"; break;
+            case '6': out += "0110"; break;
+            case '7': out += "0111"; break;
+            case '8': out += "1000"; break;
+            case '9': out += "1001"; break;
+            case 'a': out += "1010"; break;
+            case 'b': out += "1011"; break;
+            case 'c': out += "1100"; break;
+            case 'd': out += "1101"; break;
+            case 'e': out += "1110"; break;
+            case 'f': out += "1111"; break;
+            default: return "";
+        }
+    }
+
+    return out;
 }
