6d78c5d9430dc71039b3079af6b46c81339eb02c
robskrob <jewell.robertp@gmail.com>
2024-06-28 09:47:22 -0400

    decoder

--- varint.py
+++ varint.py
@@ -1,6 +1,14 @@
 import struct
 
 def encode(n):
+    """
+    while n > 0
+        take lowest order 7 bits
+        add the correct msb: 1 unless final 7 bits
+        push to some sequence of bytes
+        reduce n by 7 bits
+    return byte sequence
+    """
     out = []
     while n >0:
         part = n % 128
@@ -9,6 +17,19 @@ def encode(n):
             part |= 0x80
         out.append(part)
     return bytes(out)
+
+def decode(varn):
+    """
+    for b in varn in reverse order:
+        - shift accumulator left 7
+        - discard msb
+        - accumulate b
+    """
+    n = 0
+    for b in reversed(varn):
+        n <<= 7
+        n |= (b & 0x7f)
+    return n
 
 if __name__ == '__main__':
     cases = (
@@ -20,4 +41,5 @@ if __name__ == '__main__':
         with open(fname, 'rb') as f:
             n = struct.unpack('>Q', f.read())[0]
             assert encode(n) == expectation
+            assert decode(encode(n)) == n
     print('ok')
