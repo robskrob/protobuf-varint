399e52ea984d3c4782486b0df7d72f327057cb30
robskrob <jewell.robertp@gmail.com>
2024-06-28 10:18:01 -0400

    optimization

--- varint.py
+++ varint.py
@@ -1,30 +1,15 @@
 import struct
 
 def encode(n):
-    """
-    while n > 0
-        take lowest order 7 bits
-        add the correct msb: 1 unless final 7 bits
-        push to some sequence of bytes
-        reduce n by 7 bits
-    return byte sequence
-    """
     out = []
-    while n >0:
-        part = n % 128
+    while n > 0:
+        part = n & 0x7f
         n >>= 7
-        if n > 0:
-            part |= 0x80
+        part |= (n and 0x80 or 0x00)
         out.append(part)
     return bytes(out)
 
 def decode(varn):
-    """
-    for b in varn in reverse order:
-        - shift accumulator left 7
-        - discard msb
-        - accumulate b
-    """
     n = 0
     for b in reversed(varn):
         n <<= 7
